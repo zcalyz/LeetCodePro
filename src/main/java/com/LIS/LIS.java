8d70c821c84551d630549767d984d5c980e5b590
zhaichen <z@123>
2016-04-11 14:52:28 +0800

    最长递增子序列解法2

--- src/main/java/com/LIS/LIS.java
+++ src/main/java/com/LIS/LIS.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class LIS {
 	public static void main(String[] args) {
 		int[] array = { 2, 1, 5, 3, 6, 4, 8, 9, 7 };
-		int len = LISLength(array);
+		int len = LISLength2(array);
 		System.out.println("len: " + len);
 	}
 	
@@ -27,5 +27,35 @@ public class LIS {
 			}
 		}
 		return len;
+	}
+	
+	public static int LISLength2(int[] array){
+		int maxLenIndex = 0;
+		int[] lenArray = new int[array.length];
+		for(int i = 0; i < lenArray.length; i ++){
+			lenArray[i] = 1;
+		}
+		
+		for(int i = 1; i < lenArray.length; i++){
+			if(array[i] > array[maxLenIndex]){
+				lenArray[i] = lenArray[maxLenIndex] + 1;
+				maxLenIndex = i;
+			}else{
+				for(int j = i-1 ; j >= 0; j--){
+					if(array[j] < array[i]){
+						int tempLen = lenArray[j] + 1;
+						if(tempLen >  lenArray[i]){
+							lenArray[i] = tempLen;
+						}
+					}
+				}
+				if(lenArray[i] == lenArray[maxLenIndex] && array[i] < array[maxLenIndex]){
+					maxLenIndex = i;
+				}
+			}
+			System.out.println("maxIndexValue is " + array[maxLenIndex]);
+		}
+		System.out.println(maxLenIndex);
+		return lenArray[maxLenIndex];
 	}
 }
